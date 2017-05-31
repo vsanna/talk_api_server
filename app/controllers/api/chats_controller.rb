ceaaecc36b00fd73745514b77cc6e646849e70e2
Ryu Ishikawa <ryu.ishikawa@howtelevision.jp>
2017-05-31 10:28:29 +0900

    remove token

--- app/controllers/api/chats_controller.rb
+++ app/controllers/api/chats_controller.rb
@@ -1,6 +1,6 @@
 class Api::ChatsController < ApplicationController
   def chat
-    chat = TalkerService.new('eG6fzvdluyFD7lZ96ooy7a2kgpH05B6M')
+    chat = TalkerService.new(ENV[:R_API_TOKEN])
     begin
       @messages = chat.chat_to_array(params[:message])
     rescue => e
